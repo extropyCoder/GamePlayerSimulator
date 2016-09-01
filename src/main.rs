5f4be70e58babdc3f06ea5d5b3db1278475291a6
Laurence Kirk <laurence@extropy.io>
2016-09-01 09:20:38 +0100

    starting

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
 
+struct Player {
+    player_number : u8,
+
+}
+
+struct GameSet {
+    players : Vec<Player>
+}
+
+struct CoalitionSet {
+    players : Vec<Player>
+}
+
 fn main() {
     println!("Starting Game Player Simulation!");
     // magic happens here
+    let player1 = Player{player_number : 1};
+
+    evaluate_characteristic_function(player1);
+    evaluate_shapley();
 
 
     println!("Finished");
+
+}
+
+
+fn evaluate_characteristic_function(player : Player)->f64{
+    return 0.0;
+}
+
+fn evaluate_shapley(){
+
+
+
+
 
 }
