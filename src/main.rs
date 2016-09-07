cd5116a0e950615004a39f21d1950841ec263bd2
Laurence Kirk <laurence@extropy.io>
2016-09-07 19:31:17 +0100

    structs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 
 struct Player {
-    player_number : u8,
+    player_number : u8
 
 }
 
-struct GameSet {
-    players : Vec<Player>
+struct Coalition_Set {
+    players : Vec<Player>,
+    value : u32
 }
 
-struct CoalitionSet {
-    players : Vec<Player>
+struct Game_Set {
+    coalitions : Vec<Coalition_Set>
 }
 
 fn main() {
@@ -17,12 +18,23 @@ fn main() {
     // magic happens here
     let player1 = Player{player_number : 1};
 
+    let game_set = create_game_set();
+
     evaluate_characteristic_function(player1);
-    evaluate_shapley();
+    evaluate_shapley(game_set);
 
 
     println!("Finished");
 
+}
+
+fn create_game_set() -> Game_Set{
+let player = Player{player_number : 0};
+let players = vec![player];
+let coalition_set = Coalition_Set{players : players,value : 0};
+let coalitions = vec![coalition_set];
+
+    return Game_Set{coalitions : coalitions };
 }
 
 
@@ -30,10 +42,7 @@ fn evaluate_characteristic_function(player : Player)->f64{
     return 0.0;
 }
 
-fn evaluate_shapley(){
-}
-
-fn get_all_coalations(n : u8) -> Vec<u8>{
+fn evaluate_shapley(game_set : Game_Set){
 
 }
 
